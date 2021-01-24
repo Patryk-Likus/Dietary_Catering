9fced0a3259ca50a9b5ff8d77a830a00fcdf5195
Patryk Likus <mlodyasg1@gmail.com>
2021-01-24 10:27:19 +0100

    start project

---
+++ src/main/java/com/Dietary_Catering/Starter/DietaryCateringApplication.java
@@ -0,0 +1,13 @@
+package com.Dietary_Catering.Starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietaryCateringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietaryCateringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dietary_Catering/Starter/DietaryCateringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dietary_Catering.Starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietaryCateringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
