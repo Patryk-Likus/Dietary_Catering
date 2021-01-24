603367eaa8719edf329f0ce620d4883a09dd63c5
Lukasz <uzytkownik@kursGIT.pl>
2021-01-24 11:07:39 +0100

    Dodanie komentarza w Main.Test

--- src/main/java/com/Dietary_Catering/Starter/DietaryCateringApplication.java
+++ src/main/java/com/Dietary_Catering/Starter/DietaryCateringApplication.java
@@ -9,5 +9,5 @@ public class DietaryCateringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DietaryCateringApplication.class, args);
 	}
-
+//Komentarz Lukasza-test
 }
