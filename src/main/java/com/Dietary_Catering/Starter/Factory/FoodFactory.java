7059c01519fb34153f4d2bad9f3cf250c2e48b3c
JakubCzuchanski <68171495+JakubCzuchanski@users.noreply.github.com>
2021-02-13 23:17:52 +0100

    shop content, tooltiptext

--- src/main/java/com/Dietary_Catering/Starter/Factory/FoodFactory.java
+++ src/main/java/com/Dietary_Catering/Starter/Factory/FoodFactory.java
@@ -10,7 +10,7 @@ public class FoodFactory {
     private static List<Food> foodList = new ArrayList<Food>();
 
     static {
-        foodList.add(new Food("standard","Strandard",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
+        foodList.add(new Food("standard","Strandard",30, "Tu Będzie opis wybranej diety. "));
         foodList.add(new Food("glutenfree","Bez Glutenu",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
         foodList.add(new Food("lactosefree","Bez Laktozy",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
         foodList.add(new Food("sport","Sport",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
