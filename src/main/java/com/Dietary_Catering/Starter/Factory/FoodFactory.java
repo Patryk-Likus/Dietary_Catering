4109dd2251cbc73d43243bea283369413ee3ba87
JakubCzuchanski <68171495+JakubCzuchanski@users.noreply.github.com>
2021-02-15 18:11:16 +0100

    background color change, nav fix

--- src/main/java/com/Dietary_Catering/Starter/Factory/FoodFactory.java
+++ src/main/java/com/Dietary_Catering/Starter/Factory/FoodFactory.java
@@ -10,7 +10,7 @@ public class FoodFactory {
     private static List<Food> foodList = new ArrayList<Food>();
 
     static {
-        foodList.add(new Food("standard","Strandard",30, "Tu Będzie opis wybranej diety. "));
+        foodList.add(new Food("standard","Strandard",30, "Dieta Standard składa się z 5 posiłków. Każde danie powstaje we współpracy z naszymi dietetykami, więc możesz być pewien, że nie zabraknie w nich składników odżywczych, których potrzebuje Twój organizm. Nasi kucharze dbają, by dieta była różnorodna. Co to oznacza? W menu znajdziesz szeroki wachlarz produktów roślinnych oraz zwierzęcych, w tym nabiał, jajka, różne rodzaje mięsa oraz ryby morskie."));
         foodList.add(new Food("glutenfree","Bez Glutenu",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
         foodList.add(new Food("lactosefree","Bez Laktozy",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
         foodList.add(new Food("sport","Sport",30, "Tu Będzie opis wybranej diety. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, perferendis!"));
