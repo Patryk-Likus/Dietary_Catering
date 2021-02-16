61dacc56ac587a9b053aef0edf729f44516305d4
Patryk Likus <mlodyasg1@gmail.com>
2021-02-16 22:52:53 +0100

    authorization of logging from db with spring security and adding style

--- src/main/java/com/Dietary_Catering/Starter/DB/Repository/UserRepository.java
+++ src/main/java/com/Dietary_Catering/Starter/DB/Repository/UserRepository.java
@@ -2,7 +2,16 @@ package com.Dietary_Catering.Starter.DB.Repository;
 
 import com.Dietary_Catering.Starter.DB.Person;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository {
 
     void createUser(Person user);
+
+   Collection<Person> getAllPersons();
+
+     boolean findAccount(String email, String password);
+
 }
